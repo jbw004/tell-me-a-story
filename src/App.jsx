385506fad082fd73055d3c9babb1c28fc38fafbf
jbw004 <jeffreybweldon@gmail.com>
2024-11-18 13:24:25 -0800

    mobiel bug fix on custom template viewer (.pdf not loading(

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { pdfjs } from 'react-pdf';  // Add this import
 import { AuthProvider } from './AuthContext';
 import Header from './components/Header';
 import EditorPage from './components/EditorPage';
@@ -8,6 +9,8 @@ import CustomTemplateEditor from './components/CustomTemplateEditor';
 import MagazineDashboard from './components/MagazineDashboard';
 import CustomTemplateViewer from './components/CustomTemplateViewer';
 import './App.css';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const AppRoutes = () => {
   const location = useLocation();
